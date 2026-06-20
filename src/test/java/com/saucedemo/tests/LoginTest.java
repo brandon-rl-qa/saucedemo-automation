a14bab6dba31ae7f19082ea0fb220c25925b7912
brandon RL <brandonrl.qa@gmail.com>
2026-06-20 14:27:05 -0600

    Setup Selenium Java and Codespaces configuration

---
+++ src/test/java/com/saucedemo/tests/LoginTest.java
@@ -0,0 +1,43 @@
+package com.saucedemo.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void testLogin() {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        boolean isInventoryVisible = driver.findElement(By.className("inventory_list")).isDisplayed();
+        Assert.assertTrue(isInventoryVisible, "Inventory list should be visible after login");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
