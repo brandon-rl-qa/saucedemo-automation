b1c6915884b9b8face47195510acba8051c542d4
brandon RL <brandonrl.qa@gmail.com>
2026-06-21 10:07:26 -0600

    Add POM structure and improve README formatting (#1)
    * add POM structure and
    
    * quick start runing sample tests fix
    
    * update base readme
    
    * Fix formatting and clean up README.md
    
    Updated terminal command formatting and removed unnecessary newlines.
    
    * Fix formatting of git status command in README

--- src/test/java/com/saucedemo/tests/LoginTest.java
+++ src/test/java/com/saucedemo/tests/LoginTest.java
@@ -1,43 +1,23 @@
 package com.saucedemo.tests;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import com.saucedemo.pages.InventoryPage;
+import com.saucedemo.pages.LoginPage;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTest {
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        // options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        driver = new ChromeDriver(options);
-    }
+public class LoginTest extends BaseTest {
 
     @Test
     public void testLogin() {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-
-        boolean isInventoryVisible = driver.findElement(By.className("inventory_list")).isDisplayed();
-        Assert.assertTrue(isInventoryVisible, "Inventory list should be visible after login");
+        LoginPage login = new LoginPage(driver);
+        InventoryPage inventory = login.open().loginAs("standard_user", "secret_sauce");
+        Assert.assertTrue(inventory.isInventoryVisible(), "Inventory list should be visible after login");
     }
 
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+    @Test
+    public void testInvalidLogin() {
+        LoginPage login = new LoginPage(driver);
+        login.open().loginAs("locked_out_user", "wrong_password");
+        // could add assertions for error messages here
     }
 }
